660c84c73d8f86c5aa73cad95504959db9f75fdc
DanEnd28 <dannyendara28@gmail.com>
2022-05-31 21:33:08 -0400

    Lo logre!

--- Librerias/formulario_jquery.js
+++ Librerias/formulario_jquery.js
@@ -1,41 +1,44 @@
-$("#formulario_registro").validate({
-    rules: {
-        nombre: {
-            required: true,
-            minlength: 3,
-            maxlength: 30,
-        },
-        apellido: {
-            required: true,
-            minlength: 3,
-            maxlength: 30,
-        },
-        nacimiento: {
-            required: true,
-        }
-    }
-
-})
-
-$("#registrar").click(function() {
-    if ($("#formulario_registro").valid() == false) {
-        return;
-    }
-    let nombre = $("#nombre").val();
-    let apellido = $("#apellido").val();
-    let nacimiento = $("#nacimiento").val();
-    let genero = $("#genero").val();
-    let email = $("#email").val();
-    let nickname = $("#nickname").val();
-    let contrasena = $("#contrasena").val();
-});
-
 $(document).ready(function() {
-    $('#mostrar_contrasena').click(function() {
+    $("#mostrar_contrasena").click(function() {
         if ($('#mostrar_contrasena').is(':checked')) {
             $('#contrasena').attr('type', 'text');
         } else {
             $('#contrasena').attr('type', 'password');
         }
     });
+    $("#Registro").validate({
+        rules: {
+            nombre: {
+                required: true,
+                minlength: 3,
+                maxlength: 30,
+                Number: false
+            },
+            apellido: {
+                required: true,
+                minlength: 3,
+                maxlength: 30,
+                Number: false
+            },
+            nacimiento: {
+                required: true
+            },
+            genero: {
+                required: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            nickname: {
+                required: true
+            },
+            contrasena: {
+                required: true
+            }
+        },
+    });
+
+    $
+
 });
